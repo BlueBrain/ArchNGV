416218c253c4e39dc510d6a5eb7f95770fba410a
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2019-05-02 17:34:50 +0200

    Naive functional tests
     * build NGV based on small test circuit on GPFS
       /gpfs/bbp.cscs.ch/project/proj67/usr/share/test-build/src
    
     * compare H5 files with `h5diff` against expected output
    
    Change-Id: I7aa122ee64fe84858b7341c0e330e88002a543da

---
+++ functional_tests/compare.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+EXPECTED="$1"
+ACTUAL="$2"
+
+function h5_compare() {
+    local relpath="$1"
+    echo "Verifying $relpath..."
+    h5diff -c "$EXPECTED/$relpath" "$ACTUAL/$relpath"
+}
+
+function h5_compare_all() {
+    local dirname="$1"
+    h5_files=$(find "$EXPECTED/$dirname" -name "*.h5" -printf '%f\n' | sort)
+    for fname in $h5_files; do
+        h5_compare "$dirname/$fname"
+    done
+}
+
+h5_compare_all intermediate
+h5_compare_all morphology
+
+echo "OK!"
---
+++ functional_tests/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BUILD_DIR=build
+NGV_CONFIG=$BUILD_DIR/ngv_config.json
+
+rm -rf $BUILD_DIR
+
+mkdir $BUILD_DIR
+mkdir $BUILD_DIR/intermediate
+mkdir $BUILD_DIR/morphology
+
+sed 's;$BASE_DIR;'$(pwd)';g' ngv_config.json.template > $NGV_CONFIG
+
+python ../archngv/workflow/apps/ngv_main_workflow.py $NGV_CONFIG \
+    --cell_placement \
+    --microdomains \
+    --gliovascular_connectivity \
+    --neuroglial_connectivity \
+    --synthesis
+
+./compare.sh expected $BUILD_DIR
