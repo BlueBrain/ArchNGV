eedf7eb7f64893634ca217e91cf41fc61aa2d79b
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2019-05-23 11:29:11 +0200

    Use `morph-tool diff` for functional tests
    Change-Id: Icbc3e9c73f4188e60a63383ff800b2b881617dae

--- functional_tests/compare.sh
+++ functional_tests/compare.sh
@@ -19,9 +19,28 @@ function h5_compare_all() {
     done
 }
 
+function morph_compare() {
+    local relpath="$1"
+    echo "Verifying $relpath..."
+    morph-tool diff "$EXPECTED/$relpath" "$ACTUAL/$relpath"
+}
+
+function morph_compare_all() {
+    local dirname="$1"
+    h5_files=$(find "$EXPECTED/$dirname" -maxdepth 1 -name "*.h5" -printf '%f\n' | sort)
+    for fname in $h5_files; do
+        if [[ $fname =~ 'annotation' ]]; then
+           h5_compare "$dirname/$fname"
+        else
+           morph_compare "$dirname/$fname"
+        fi
+    done
+}
+
 h5_compare_all "."
 h5_compare_all "./microdomains"
-h5_compare_all "./morphologies"
 h5_compare_all "./sonata"
+
+morph_compare_all "./morphologies"
 
 echo "OK!"
