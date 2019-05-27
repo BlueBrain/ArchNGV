01060794d3b5b0b04c8521660086a1e04e631d69
Zisis Eleftherios <eleftherios.zisis@epfl.ch>
2019-05-27 17:19:15 +0200

    relaxed alignment check
    Change-Id: I35a7f4996024ef6560ae75249dcda084b7bdc9dd

--- archngv/core/checks.py
+++ archngv/core/checks.py
@@ -1,22 +1,40 @@
 """ Check functions for cell placement
 """
+import logging
+import numpy as np
 from .exceptions import NotAlignedError
 
 
-def assert_bbox_alignment(bbox1, bbox2):
+L = logging.getLogger(__name__)
+
+
+def assert_bbox_alignment(bbox1, bbox2, tolerance=10.0):
     """ Checks if bounding boxes are aligned
 
     Arguments:
         bbox1: BoundingBox
         bbox2: BoundingBox
+        tolerance: float
+            Alignment tolerance in micrometers
 
     Raises:
         NotAllignedError if bounding boxes are not aligned
     """
-    if bbox1 != bbox2:
-        msg = ('Max Point\tMin Point\n' +
-               '[{:.2f} {:.2f} {:.2f}]\t'.format(*bbox1.min_point) +
+    def message(bbox1, bbox2):
+        return ('\n' +
+               'Min points: [{:.2f} {:.2f} {:.2f}]\t'.format(*bbox1.min_point) +
                '[{:.2f} {:.2f} {:.2f}]\n'.format(*bbox2.min_point) +
-               '[{:.2f} {:.2f} {:.2f}]\t'.format(*bbox1.max_point) +
+               'Max Points: [{:.2f} {:.2f} {:.2f}]\t'.format(*bbox1.max_point) +
                '[{:.2f} {:.2f} {:.2f}]\n'.format(*bbox2.max_point))
-        raise NotAlignedError(msg)
+
+    if not bbox1 == bbox2:
+
+        max_aligned = np.allclose(bbox1.max_point, bbox2.max_point, atol=tolerance)
+        min_aligned = np.allclose(bbox1.min_point, bbox2.min_point, atol=tolerance)
+
+        msg = message(bbox1, bbox2)
+
+        if max_aligned and min_aligned:
+            L.warning('Datasets aligned within tolerance %f um\n%s', tolerance, msg)
+        else:
+            raise NotAlignedError(msg)
--- archngv/core/tests/test_checks.py
+++ archngv/core/tests/test_checks.py
@@ -21,20 +21,47 @@ def test_assert_bbox_alignment__aligned():
     assert_bbox_alignment(bbox1, bbox3)
 
 
+def test_check_alignment__tolerance():
+
+    tolerance = 10.0
+
+    bbox1 = BoundingBox(np.array([0., 0., 2.]),
+                        np.array([8., 9., 10.]))
+
+    bbox2 = BoundingBox(np.array([1., 2., 3.]),
+                        np.array([17., 9., 10.]))
+
+    bbox3 = BoundingBox(np.array([0., 8., 2.]),
+                        np.array([8., 9., 10.]))
+
+    assert_bbox_alignment(bbox1, bbox3, tolerance)
+    assert_bbox_alignment(bbox1, bbox2, tolerance)
+    assert_bbox_alignment(bbox2, bbox3, tolerance)
+
+    with pytest.raises(NotAlignedError):
+        assert_bbox_alignment(bbox1, bbox3, tolerance=7.0)
+
+    with pytest.raises(NotAlignedError):
+        assert_bbox_alignment(bbox1, bbox2, tolerance=8.0)
+
+    with pytest.raises(NotAlignedError):
+        assert_bbox_alignment(bbox2, bbox3, tolerance=5.0)
+
+
 def test_check_alignment__not_aligned():
 
     min_point = np.array([1., 2., 3.])
     max_point = np.array([8., 9., 10.])
 
     bbox1 = BoundingBox(min_point, max_point)
-    bbox2 = BoundingBox(min_point, max_point - 1.0)
+    bbox2 = BoundingBox(min_point, max_point - 13.0)
     bbox3 = BoundingBox(min_point, max_point)
 
     assert_bbox_alignment(bbox1, bbox3)
 
     with pytest.raises(NotAlignedError):
-        assert_bbox_alignment(bbox1, bbox2)
+        assert_bbox_alignment(bbox1, bbox2, 10.)
 
     with pytest.raises(NotAlignedError):
-        assert_bbox_alignment(bbox2, bbox3)
+        assert_bbox_alignment(bbox2, bbox3, 10.)
 
