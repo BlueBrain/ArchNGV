61f5d47b8aa42da554e440f1fef86ef806ce0957
Mike Gevaert <michael.gevaert@epfl.ch>
2019-08-16 11:00:18 +0200

    added global indices test for area_fitting
    Change-Id: Ib8c34dcb83e0fb5efa24569b46c57105f7995b5d

--- archngv/core/endfeet_area_reconstruction/tests/test_area_fitting.py
+++ archngv/core/endfeet_area_reconstruction/tests/test_area_fitting.py
@@ -1,14 +1,16 @@
+from numpy.testing import assert_allclose
 import utils
 import numpy as np
 from archngv.core.endfeet_area_reconstruction.detail import area_fitting, endfoot
 
 
 def test_area_fit():
-    ef = endfoot.create_endfoot_from_global_data(0,
-                                                 *utils.create_mesh_data(height=10,
-                                                                         width=10))
+    world_offset = 111
+    height = width = 10
+    ef = endfoot.create_endfoot_from_global_data(
+        0, *utils.create_mesh_data(height, width, world_offset))
     ef.extra = {'vertex':
-                {'travel_times': np.arange(ef.number_of_triangles,
+                {'travel_times': np.arange(world_offset + ef.number_of_triangles,
                                            dtype=np.float32),
                  },
                 }
@@ -17,4 +19,8 @@ def test_area_fit():
     assert ef.number_of_vertices == 49
     assert ef.number_of_triangles == 70
     assert ef.area == 35.0
-    assert ef.local_to_global_map == {i: i for i in range(ef.number_of_vertices)}
+    assert ef.local_to_global_map == {i: world_offset + i
+                                      for i in range(ef.number_of_vertices)}
+    assert_allclose(ef.vasculature_vertices,
+                    [world_offset + i for i in range(ef.number_of_vertices)])
+
