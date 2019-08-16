b0a00d9b0cea24fee0508d7ecf654866f9cff952
Mike Gevaert <michael.gevaert@epfl.ch>
2019-08-16 09:56:37 +0200

    make sure endfoot tests account for world indicies
    Change-Id: I3b48d2585f3b379a08eb5aa0acb9ee772cee404f

--- archngv/core/endfeet_area_reconstruction/tests/test_endfoot.py
+++ archngv/core/endfeet_area_reconstruction/tests/test_endfoot.py
@@ -1,24 +1,28 @@
+from numpy.testing import assert_allclose
 import utils
 from archngv.core.endfeet_area_reconstruction.detail import endfoot
 
 
-def create_data(height=10, width=10):
+def _create_data(height=10, width=10, world_offset=0):
     ef = endfoot.create_endfoot_from_global_data(0,
                                                  *utils.create_mesh_data(height,
-                                                                         width))
+                                                                         width,
+                                                                         world_offset))
     return ef
 
 
 def test_EndFoot():
     height = width = 10
-    ef = create_data(height, width)
+    world_offset = 101
+    ef = _create_data(height, width, world_offset)
     #ef.coordinates_array
     #ef.triangle_array
     #ef.edges
 
-    assert ef.local_to_global_map == {i: i for i in range(ef.number_of_vertices)}
+    global_vertices = list(range(world_offset, world_offset + ef.number_of_vertices))
+    assert ef.local_to_global_map == dict(enumerate(global_vertices))
 
-    assert list(ef.vasculature_vertices) == list(range(ef.number_of_vertices))
+    assert list(ef.vasculature_vertices) == global_vertices
 
     assert ef.number_of_vertices == height * width
     assert ef.number_of_triangles == (height - 1.) * (width - 1.) * 2.
@@ -37,16 +41,22 @@ def test_EndFoot():
 
 def test_EndFoot_shrink():
     #remove all
-    ef = create_data()
+    ef = _create_data()
     ef.shrink(set(range(ef.number_of_vertices)))
     assert ef.area == 0
     assert ef.number_of_vertices == 0
     assert ef.number_of_triangles == 0
+    assert len(ef.vasculature_vertices) == 0
 
     #remove left hand triangle strip
     height = width = 10
-    ef = create_data(height, width)
+    world_offset = 111
+    ef = _create_data(height, width, world_offset)
     ef.shrink(set(range(0, height * width, width)))
     assert ef.area == (height - 1) * (width - 2)
     assert ef.number_of_vertices == height * (width - 1)
     assert ef.number_of_triangles == (height - 1) * (width - 2) * 2
+
+    assert_allclose(ef.vasculature_vertices,
+                    list(set(range(world_offset, world_offset + height * width)) -
+                         {111, 121, 131, 141, 151, 161, 171, 181, 191, 201}))
--- archngv/core/endfeet_area_reconstruction/tests/utils.py
+++ archngv/core/endfeet_area_reconstruction/tests/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def create_mesh_data(height=10, width=10):
+def create_mesh_data(height=10, width=10, world_offset=0):
     '''
     Build a triangle strip:
     Vertices are layed out left to right, bottom to top:
@@ -21,8 +21,9 @@ def create_mesh_data(height=10, width=10):
     triangles = np.vstack((lower_triangles, upper_triangles))
     mesh_triangles = (np.repeat(np.arange(height - 1), len(triangles))[:, None] * [width, width, width] +
                       np.tile(triangles, (height - 1, 1))).astype(np.uint64)
+    mesh_triangles += world_offset
 
     mesh_coordinates = np.transpose([np.tile(np.arange(width), height), np.repeat(np.arange(height), width)])
-    mesh_coordinates = np.vstack((mesh_coordinates.T, [1] * len(mesh_coordinates))).T.astype(np.float32)
-
+    mesh_coordinates = np.vstack((mesh_coordinates.T, [1] * len(mesh_coordinates))).T
+    mesh_coordinates = np.vstack((np.zeros((world_offset, 3)), mesh_coordinates)).astype(np.float32)
     return mesh_coordinates, mesh_triangles, np.unique(mesh_triangles)
