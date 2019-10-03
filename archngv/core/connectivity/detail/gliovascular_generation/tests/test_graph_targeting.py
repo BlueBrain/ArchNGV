097032e76c6d7816c516ed32b3ecb8d5e96955f8
Mike Gevaert <michael.gevaert@epfl.ch>
2019-10-03 15:04:52 +0200

    move graph_targetting to python
    * direct port
    * slight slowdown, but it runs in under a second on the datasets, and
      only once in the generation of a circuit, afaik
    * added test
    
    Change-Id: Ifdb200f8596e51581b2ae84fdc3fb00f385b24e9

--- archngv/core/connectivity/detail/gliovascular_generation/tests/test_graph_targeting.py
+++ archngv/core/connectivity/detail/gliovascular_generation/tests/test_graph_targeting.py
@@ -1,6 +1,7 @@
+from numpy.testing import assert_allclose
 import numpy as np
 
-from archngv.core.connectivity.detail.gliovascular_generation import graph_targeting as _gt
+from archngv.core.connectivity.detail.gliovascular_generation import graph_targeting
 
 
 def parametric_line(start, u_dir, t):
@@ -24,20 +25,24 @@ def _create_test_line(Npoints, dP, target_linear_density):
 
     u_d = direction / np.linalg.norm(direction)
 
-    points = np.asarray([parametric_line(start, u_d, t) for t in dP * np.arange(0., float(Npoints))], dtype=np.float64)
+    points = np.asarray([parametric_line(start, u_d, t)
+                         for t in dP * np.arange(0., float(Npoints))],
+                        dtype=np.float64)
 
     edges = sequential_edges(Npoints)
 
     Ntargets = int(np.round(line_lengths(points, edges).sum() * l_T))
 
-    targets = np.asarray([parametric_line(start, u_d, t) for t in  (1. / l_T) * np.arange(0., float(Ntargets))], dtype=np.float)
+    targets = np.asarray([parametric_line(start, u_d, t)
+                          for t in (1. / l_T) * np.arange(0., float(Ntargets))],
+                         dtype=np.float)
 
     return points, edges, targets
 
 
 def test_targeting_on_straight_line():
 
-    def _format_ouput(a_targets, e_targets):
+    def _format_output(a_targets, e_targets):
         return ("\n Mismatch in target point generation "
                 "\n\n Actual: \n{0},\n\n Expected: \n{1}").format(a_targets, e_targets)
 
@@ -45,9 +50,8 @@ def test_targeting_on_straight_line():
 
     points, edges, e_targets = _create_test_line(100, 2.3, linear_density)
 
-    parameters = {"linear_density": linear_density}
-
-    a_targets, a_segments = _gt.distribution_on_line_graph(points[edges[:, 0]], points[edges[:, 1]], linear_density)
+    a_targets, a_segments = graph_targeting._distribution_on_line_graph(
+        points[edges[:, 0]], points[edges[:, 1]], linear_density)
 
     assert np.allclose(a_targets, e_targets), _format_output(a_targets, e_targets)
 
@@ -61,10 +65,8 @@ def test_targeting_on_random_lines():
 
         points, edges, e_targets = _create_test_line(20, L, linear_density)
 
-        parameters = {"linear_density": linear_density}
-
-        a_targets, a_segments = \
-            _gt.distribution_on_line_graph(points[edges[:, 0]], points[edges[:, 1]], linear_density)
+        a_targets, a_segments = graph_targeting._distribution_on_line_graph(
+            points[edges[:, 0]], points[edges[:, 1]], linear_density)
 
         txt = "\nRandomized line test Failed: \n"
         txt += "parameters: L = {0}, linear density = {1}\n".format(L, linear_density)
@@ -73,3 +75,21 @@ def test_targeting_on_random_lines():
         assert len(a_targets) == len(e_targets), txt
         assert np.allclose(a_targets, e_targets), txt
 
+
+def test_create_targets():
+    points = np.array([[0, 0, 0],
+                       [0, 0, 10],
+                       [0, 0, 19],
+                       [0, 0, 21],
+                       ], dtype=np.float64)
+    edges = np.array([[0, 1],
+                      [1, 2],
+                      [2, 3],
+                      ])
+    positions, segments = graph_targeting.create_targets(points, edges, {'linear_density': 0.25})
+    assert_allclose(positions, np.array([[ 0.,  0.,  0.],
+                                         [ 0.,  0.,  4.],
+                                         [ 0.,  0.,  8.],
+                                         [ 0.,  0., 12.],
+                                         [ 0.,  0., 16.]]))
+    assert_allclose(segments, np.array([0, 0, 0, 1, 1], dtype=np.uint64))
