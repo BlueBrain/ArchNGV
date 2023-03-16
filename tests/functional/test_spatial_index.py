99b509f2853e5c3d114ee39af3cde6cb30693148
Jean Jacquemier <jean.jacquemier@epfl.ch>
2023-03-16 14:05:48 +0100

    Remove unwanted print

--- tests/functional/test_spatial_index.py
+++ tests/functional/test_spatial_index.py
@@ -9,7 +9,6 @@ BUILD_DIR = Path(__file__).parent.resolve() / "build"
 
 def test_synapses():
     index_path = BUILD_DIR / 'spatial_index_synapses/meta_data.json'
-    print(f'index_payth {type(index_path)} ')
     index = spatial_index.open_index(index_path.as_posix())
     assert len(index) > 0
     assert index.element_type == 'synapse'
