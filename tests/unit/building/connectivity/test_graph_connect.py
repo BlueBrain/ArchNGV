5ec1fdc385067c1dfa3922dedff56dae18795d5a
Jean Jacquemier <jean.jacquemier@epfl.ch>
2023-03-17 15:23:28 +0100

    Mock the randomize result on building/connectivity/test_graph_connect.py for the unit test

--- tests/unit/building/connectivity/test_graph_connect.py
+++ tests/unit/building/connectivity/test_graph_connect.py
@@ -1,7 +1,7 @@
+from mock import patch
 import numpy as np
-import pytest
-import pandas as pd
 import numpy.testing as npt
+import pandas as pd
 from pathlib import Path
 
 from archngv.core.datasets import Microdomains
@@ -11,30 +11,36 @@ from archngv.building.connectivity.detail.gliovascular_generation import graph_c
 DATA_DIR = Path(__file__).resolve().parent / "../../app/data"
 BUILD_DIR = DATA_DIR / "frozen-build"
 
-
-def test_domains_to_vasculature():
-    cell_ids = np.array([0, 1, 2, 3, 4], dtype=np.int16)
+@patch('scipy.stats._distn_infrastructure.rv_continuous_frozen.rvs')
+def test_domains_to_vasculature(mocked_function):
+    mocked_function.return_value = np.array([2, 1] )
+    cell_ids = np.array([0, 1 ], dtype=np.int16)
     reachout_strategy_function = reachout_strategy_function = strategy("maximum_reachout")
+
     potential_targets = pd.DataFrame(
         {
-            "x": [26.0, 26.0, 30.0, 33.0],
-            "y": [23.0, 26.0, 30.0, 33.0],
-            "z": [23.0, 26.0, 30.0, 33.0],
-            "r": [2.96, 2.69, 2.65, 2.65],
-            "edge_index": [0, 1, 1, 2],
-            "vasculature_section_id": [0, 1, 1, 1],
-            "vasculature_segment_id": [0, 0, 1, 1],
+            "x": [0.0 , 0.0 , 40.0],
+            "y": [20.0, 40.0, 40.0],
+            "z": [40.0, 60.0, 30.0],
+            "r": [0., 0., 0.],
+            "edge_index": [0, 1, 1],
+            "vasculature_section_id": [0, 1, 1],
+            "vasculature_segment_id": [0, 0, 1],
         }
     )
-
+    
     properties = {"reachout_strategy": "maximum_reachout", "endfeet_distribution": [2, 2, 0, 15]}
-
     domains = Microdomains(BUILD_DIR / "microdomains.h5")
+        
+    """ This is the omain Bounding Box values
+    (-7.6956673, 33.304604, 20.442867, 77.779236, 77.40124, 77.37364)
+    (-7.914425, 7.0291786, -7.3803453, 77.56047, 77.630165, 47.34516)
+    """
 
     astrocyte_target_edges = tested.domains_to_vasculature(
         cell_ids, reachout_strategy_function, potential_targets, domains, properties
     )
 
     print(f"astrocyte_target_edges {astrocyte_target_edges}")
-    npt.assert_array_equal(astrocyte_target_edges, [[1, 1],  [1, 3],  [1, 0],  [3, 0],  [3, 1],  [3, 3]])
-                                                    
+    npt.assert_array_equal(astrocyte_target_edges, [[0, 1], [1, 2]])
+
