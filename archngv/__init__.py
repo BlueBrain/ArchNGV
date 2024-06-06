74004c53e2735c06d1f3606ff0b2962b81396ae6
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2024-06-06 12:40:46 +0200

    Switch to modern pyproject.toml

--- archngv/__init__.py
+++ archngv/__init__.py
@@ -2,4 +2,8 @@
 
 """ Archngv, the pipeline for the Neuronal - Glial - Vascular architecture
 """
+from importlib.metadata import version
+
+__version__ = version(__package__)
+
 from archngv.core.circuit import NGVCircuit
--- archngv/version.py
+++
@@ -1,4 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-""" archngv version """
-VERSION = "3.2.0.dev0"
